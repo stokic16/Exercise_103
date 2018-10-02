9914189bf5150fdf1339d87001ee4e126232b968
Kilian <kiliansc5@gmail.com>
2018-10-02 09:05:10 +0200

    Added and cancel Button now working

--- Exercise_103/src/AppointmentDlg.java
+++ Exercise_103/src/AppointmentDlg.java
@@ -20,7 +20,8 @@ public class AppointmentDlg extends javax.swing.JDialog {
     private boolean ok=false;
     private Appointment termin;
     private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
-    public AppointmentDlg() {
+    public AppointmentDlg(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
         initComponents();
     }
 
@@ -95,6 +96,7 @@ public class AppointmentDlg extends javax.swing.JDialog {
      */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
+         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
@@ -117,10 +119,17 @@ public class AppointmentDlg extends javax.swing.JDialog {
         }
         //</editor-fold>
 
-        /* Create and display the form */
+        /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new AppointmentDlg().setVisible(true);
+                AppointmentDlg dialog = new AppointmentDlg(new javax.swing.JFrame(), true);
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
             }
         });
     }
